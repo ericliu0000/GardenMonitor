b4edda7880caaae552e1fafc7fd9014b0a53b208
ericliu0000 <55063842+ericliu0000@users.noreply.github.com>
2021-05-18 08:10:25 -0400

    Edit schedule tag name

--- website/main.py
+++ website/main.py
@@ -26,8 +26,7 @@ def water_amount():
 @app.route('/Schedule.html', methods=["GET", "POST"])
 def schedule():
     if request.method == "POST":
-        print("Got a form response")
-        print(request.args)
+        print(request.form.getlist("Sun12am"))
     return render_template("Schedule.html")
 
 
