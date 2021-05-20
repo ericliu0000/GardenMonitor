6490ddf4b1da550747b968d1d7b0cdba9958c4eb
ericliu0000 <55063842+ericliu0000@users.noreply.github.com>
2021-05-20 08:32:27 -0400

    New schedule using text entry

--- Experimental/main.py
+++ Experimental/main.py
@@ -2,31 +2,15 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 import datetime
 
-
 app = Flask(__name__)
+
+global interval
+interval = 6
 
 
 @app.route("/")
 def index():
-    return redirect("/Home.html")
-
-
-@app.route("/Home.html")
-def home():
-    currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template("/Home.html", time=currentTime)
-
-
-@app.route("/Changelog.html")
-def changelog():
-    currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template("/Changelog.html", time=currentTime)
-
-
-@app.route("/Water Amount.html")
-def water_amount():
-    currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template("/Water Amount.html", time=currentTime)
+    return redirect("/Schedule.html")
 
 
 @app.route('/Schedule.html', methods=["GET", "POST"])
@@ -40,7 +24,7 @@ def schedule():
             print(interval)
             return render_template("/Schedule.html", time=currentTime, status=f"Successfully set interval to {interval} hours.")
         except:
-            return render_template("/Schedule.html", time=currentTime, status=f"Something went wrong! Interval still at {interval} hours.")
+            return render_template("/Schedule.html", time=currentTime, status="Something wrong!")
     return render_template("/Schedule.html", time=currentTime, status=f"Interval is {interval} hours.")
 
 
