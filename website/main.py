a9e78d01b7c40cd2a033d5581fa9f04404ee6816
YoBaka-1 <wujin1352@gmail.com>
2021-05-21 09:18:17 -0400

    Water Amount
    Added water amount, doesn't show the initial message when page is opened.

--- website/main.py
+++ website/main.py
@@ -38,11 +38,25 @@ def schedule():
         try:
             interval = int(request.form["text"])
             print(interval)
-            return render_template("/Schedule.html", time=currentTime, status=f"Successfully set interval to {interval} hours.")
+            return render_template("/Schedule.html", time = currentTime, status = f"Successfully set interval to {interval} hours.")
         except:
-            return render_template("/Schedule.html", time=currentTime, status=f"Something went wrong! Interval still at {interval} hours.")
-    return render_template("/Schedule.html", time=currentTime, status=f"Interval is {interval} hours.")
+            return render_template("/Schedule.html", time = currentTime, status = f"Something went wrong! Interval still at {interval} hours.")
+    return render_template("/Schedule.html", time = currentTime, status = f"Interval is {interval} hours.")
+
+@app.route('/Water Amount.html', methods = ["GET", "POST"])
+def waterAmount():
+    amount = 0
+    currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if request.method == "POST":
+        print("Got a response")
+        try:
+            amount = int(request.form["text"])
+            print(amount)
+            return render_template("/Water Amount.html", time = currentTime, status = f"Successfully set Water Amount to {amount} mL")
+        except:
+            return render_template("/Water Amount.html", time = currentTime, status = f"Something went wrong! Water Amount still at {amount} mL")
+    return render_template("/Water Amount.html", time = currentTime, status = f"Water Amount is {amount} mL")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug = True, host = "0.0.0.0")
